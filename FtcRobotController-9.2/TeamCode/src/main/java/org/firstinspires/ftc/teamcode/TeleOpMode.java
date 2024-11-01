66d71f0fc08dc211ec89daa92d0f259cb4b29b63
AarushTripathi <106599323+AarushTripathi@users.noreply.github.com>
2024-11-01 14:05:31 -0400

    Changes to TeleOP that might work :).

--- FtcRobotController-9.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ FtcRobotController-9.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -11,6 +11,7 @@ public class TeleOpMode extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
+        // Is motors
         DcMotor frontLeftMotor = hardwareMap.dcMotor.get("lf_drive");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("rf_drive");
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("lb_drive");
@@ -18,19 +19,24 @@ public class TeleOpMode extends LinearOpMode {
         DcMotor linearSlideMotor = hardwareMap.dcMotor.get("arm1");
         DcMotor slideMovementMotor = hardwareMap.dcMotor.get("movement1");
 
+        // Is servo
+        Servo armServo = hardwareMap.servo.get("servo_arm");
 
-
-        // Constants for motor power
+        // Pow
         final double STOP_POWER = 0;
         final double UP_POWER = 0.2;
         final double DOWN_POWER = -0.5;
 
-        // Configure linear slide motor
+        // Servo positions
+        final double SERVO_OPEN_POSITION = 0.2;   // Adjust this position as needed
+        final double SERVO_CLOSED_POSITION = 0.8; // Adjust this position as needed
+
+        // LSlide
         linearSlideMotor.setDirection(DcMotor.Direction.FORWARD);
         linearSlideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         linearSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        // Define slide positions
+        // Pos
         final int SLIDE_LOW_POSITION = 0;
         final int SLIDE_MID_POSITION = 1000;
         final int SLIDE_HIGH_POSITION = 2000;
@@ -76,8 +82,15 @@ public class TeleOpMode extends LinearOpMode {
 
             slideMovementMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+            // Gamepad2 - Servo Controls
+            if (gamepad2.dpad_up) {
+                armServo.setPosition(SERVO_OPEN_POSITION);
+            } else if (gamepad2.dpad_down) {
+                armServo.setPosition(SERVO_CLOSED_POSITION);
+            }
 
             telemetry.addData("Slide Position", linearSlideMotor.getCurrentPosition());
+            telemetry.addData("Servo Position", armServo.getPosition());
             telemetry.update();
         }
     }
